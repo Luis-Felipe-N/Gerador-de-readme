3376334a9a1a72752e27af807b7a8aff703771d6
luis.nunes <luis.nunes@mail.uft.edu.br>
2022-08-23 22:00:31 -0300

    function fetch repo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import type { NextPage } from 'next'
-// import styles from '../styles/Home.module.scss'
+import Image from 'next/image'
+import style from '../styles/Home.module.scss'
 import { Header } from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <main className={style.home}>
+        <figure className={style.card}>
+          <Image
+            src="/capa.svg"
+            width={320}
+            height={240}
+            alt=""
+            objectFit='cover'
+            objectPosition="center"
+          />
+          <figcaption>
+            <strong>Editor markdown</strong>
+            <time>21 de ago</time>
+          </figcaption>
+        </figure>
+    </main>
   )
 }
 
